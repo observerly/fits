54fb921cc241be68e2b7686e1d44817865b05bea
Michael Roberts <michael@observerly.com>
2022-01-14 16:45:33 +0000

    feat: Improved TS for the readBlockAsArrayBuffer() call.
    feat: Improved TS for the readBlockAsArrayBuffer() call. Added 's' to the FITSBlock interface which holds the buffer as a raw parsed string.

--- src/utilities/readBlockAsArrayBuffer.ts
+++ src/utilities/readBlockAsArrayBuffer.ts
@@ -10,9 +10,9 @@
  * @returns a Promise which resolves the callback of the callback function provided
  */
 export const readBlockAsArrayBuffer = async (
-  block: File,
-  callback: (result: ArrayBuffer) => unknown
-) => {
+  block: Blob,
+  callback: (result: ArrayBuffer) => { s: string; end: boolean; parsed: boolean }
+): Promise<{ s: string; end: boolean; parsed: boolean }> => {
   return new Promise((resolve, reject) => {
     const reader = new FileReader()
 
@@ -20,6 +20,12 @@ export const readBlockAsArrayBuffer = async (
       if (e.target?.result && e.target.result instanceof ArrayBuffer) {
         resolve(callback(e.target.result))
       }
+
+      resolve({
+        s: '',
+        end: false,
+        parsed: false
+      })
     }
 
     reader.onerror = reject
